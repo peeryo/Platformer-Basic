9c1b9fe9d8a457d0251659c39fa052afee175627
[peeryo] <peeryo@sfusd.edu>
2024-04-20 12:32:06 -0700

    Video 2 complete
    The tilemap and tileset feature are finicky. I had to save quit and reopen before they worked for painting on the terrain

--- scripts/Player/player.gd
+++ scripts/Player/player.gd
@@ -12,12 +12,18 @@ var direction :
 func _physics_process(delta):
 	var x_input = Input.get_axis("btn_left", "btn_right")
 	direction = x_input
-	velocity.x = x_input * 90
 	
-	if x_input == 0:
+	if Input.is_action_just_pressed(" btn_jump") and is_on_floor():
+		velocity.y = -300
+		sprite.play("jump")
+	elif not is_on_floor() and velocity.y >= 0:
+		sprite.play("fall")
+	elif x_input == 0 and is_on_floor():
 		sprite.play("idle")
-	else:
+	elif is_on_floor():
 		sprite.play("run")
-	
+		
+	velocity.x = x_input * 90
+	velocity.y += 900 * delta
 	move_and_slide()
 	
